ab568080baa974c09d5175c50471a86b116170a1
bodryjhryap <senya.akimov@mail.ru>
2023-08-11 15:04:01 +0500

    Master

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car extends Interface{
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
